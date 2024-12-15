b020cfab011e54cc5478b54bd740589f3f1c3be6
13Anisha <119130741+13Anisha@users.noreply.github.com>
2024-12-16 00:55:00 +0530

    added database for storing user data

--- SehatKal/src/Components/Login.jsx
+++ SehatKal/src/Components/Login.jsx
@@ -1,123 +1,127 @@
-import React, {useState,useContext } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { AiFillBook, AiFillGoogleCircle } from 'react-icons/ai';
-import { CiLogin } from "react-icons/ci";
-import { AuthContext } from '../Components/AuthProvider';
-import "./loginstyle.css";
+import { useState } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { AiFillGoogleCircle } from 'react-icons/ai';
+import { CiLogin } from 'react-icons/ci';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import './loginstyle.css';
+import OAuth from './OAuth';
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  const { login, loginwithGoogle } = useContext(AuthContext);
-
-  const location = useLocation();
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [error, setError] = useState('');
+  const { loading } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const from = location.state?.from?.pathname || "/";
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/';
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const userCredential = await login(email, password);
-      navigate("/", { replace: true });
-    } catch (error) {
-      handleError(error);
-    }
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
 
-  const handleRegister = async () => {
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    const { email, password } = formData;
+
+    if (!email || !password) {
+      return setError('Please fill out all fields.');
+    }
+
+    setError('');
+    dispatch(signInStart());
+
     try {
-      const result = await loginwithGoogle();
+      const res = await fetch('http://localhost:3000/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        return setError(data.message || 'Something went wrong. Please try again.');
+      }
+
+      dispatch(signInSuccess(data));
       navigate(from, { replace: true });
     } catch (error) {
-      handleError(error);
+      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
 
-  const handleError = (error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    switch (errorCode) {
-      case 'auth/invalid-credential':
-        setError('There seems to be an issue with your login credentials. Please double-check your email and password, or reset your password if necessary.');
-        break;
-      case 'auth/wrong-password':
-        setError('Incorrect email or password.');
-        break;
-      case 'auth/user-not-found':
-        setError('The email address you entered is not associated with an account.');
-        break;
-      default:
-        setError(errorMessage);
-    }
-  };
+ 
 
   return (
-    <div className='row g-0 justify-content-center align-items-center vh-100 login-container'>
-      <div className="col-10 row g-0 border rounded-2 bg-white">
+    <div className="row g-0 justify-content-center align-items-center login-container mt-3">
+      <div className="col-10 row g-0 border rounded-2 bg-white mb-8">
+        {/* Left Section */}
         <div className="d-none d-md-block col-6">
-          <img src="./login.jpg" alt="" className='img-fluid h-100' />
+          <img src="./login.jpg" alt="" className="img-fluid h-100" />
         </div>
-        <form className='col-12 col-md-6 px-3 py-4' onSubmit={handleLogin}>
-          <div className='d-flex align-items-center justify-content-center'>
-            <h5 className='text-center py-2 mb-1 fst-italic fw-lighter'>Discover, Explore, Imagine.</h5>
-            <AiFillBook />
-          </div>
 
-          {error && (
-            <div className="alert alert-danger" role="alert">
-              {error}
-            </div>
-          )}
+        {/* Form Section */}
+        <form className="col-12 col-md-6 px-3 py-3" onSubmit={handleSignIn}>
+          
 
+          {error && <div className="alert alert-danger text-center mb-3">{error}</div>}
+
+          <h5 className="text-center fst-italic mb-3">Login</h5>
+
+          {/* Email Input */}
           <div className="form-floating mb-3">
             <input
               type="email"
-              className='form-control rounded'
-              id='email'
-              placeholder='xyz.@gmail.com'
+              className="form-control rounded"
+              id="email"
+              placeholder="xyz.@gmail.com"
+              value={formData.email}
+              onChange={handleChange}
               required
-              onChange={(e) => setEmail(e.target.value)}
             />
             <label htmlFor="email">Email</label>
           </div>
 
+          {/* Password Input */}
           <div className="form-floating mb-3">
             <input
               type="password"
-              className='form-control rounded'
-              id='password'
-              placeholder='password'
+              className="form-control rounded"
+              id="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
               required
-              onChange={(e) => setPassword(e.target.value)}
             />
             <label htmlFor="password">Password</label>
           </div>
 
+          {/* Submit Button */}
           <div className="text-center">
-            <button className='btn login-btn py-2 px-2 rounded-3' type="submit">
-              Login 
-            </button>
-          </div>
-
-          <div className="text-center mt-3">
             <button
-              className='btn btn-outline-dark w-100 d-flex align-items-center justify-content-center'
-              type="button"
-              onClick={handleRegister}
+              type="submit"
+              className="btn login-btn py-2 px-2 rounded-3 d-flex align-items-center justify-content-center"
+              disabled={loading}
             >
-              <AiFillGoogleCircle className='w-8 h-8 mr-2' />
-              Continue with Google
+              {loading ? (
+                <span>Loading...</span>
+              ) : (
+                <>
+                  Login <CiLogin />
+                </>
+              )}
             </button>
           </div>
 
-          <div className="text-center mt-4">
-            Don't have an Account? <Link to="/signup" className='loginLink'>Sign Up</Link>
-          </div>
+         <OAuth/>
 
-          <div className='text-center mt-4'>
-            By continuing, I agree to the <Link className='Terms'> Terms of Use </Link> & <Link className='Privacy'>Privacy Policy</Link>
+          {/* Footer Links */}
+          <div className="text-center mt-4">
+            Don't have an Account? <Link to="/signup" className="loginLink">Sign Up</Link>
+          </div>
+          <div className="text-center mt-4">
+            By continuing, I agree to the <Link className="Terms">Terms of Use</Link> & <Link className="Privacy">Privacy Policy</Link>
           </div>
         </form>
       </div>
--- SehatKal/src/Components/SignUp.jsx
+++ SehatKal/src/Components/SignUp.jsx
@@ -1,155 +1,144 @@
-import React,{useContext, useState} from 'react';
-import {Link, useLocation, useNavigate} from 'react-router-dom';
-import { AiFillBook, AiFillGoogleCircle} from 'react-icons/ai';
-import { CiLogin } from "react-icons/ci";
-import "../Components/loginstyle.css";
-import { AuthContext } from '../Components/AuthProvider';
-
-
+import React, { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AiFillBook, AiFillGoogleCircle } from 'react-icons/ai';
+import { CiLogin } from 'react-icons/ci';
+import '../Components/loginstyle.css';
+import OAuth from './OAuth';
 
 export default function SignUp() {
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
-    const [username,setUsername]=useState("");
+  const [formData, setFormData] = useState({ username: '', email: '', password: '' });
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const {createUser,loginwithGoogle}=useContext(AuthContext);
-    const [error,setError]=useState("");
-    
-    const location=useLocation();
-    const navigate=useNavigate();
+  const from = location.state?.from?.pathname || '/';
 
-    const from=location.state?.from?.pathname|| "/";
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
 
-    
-    const handleSignUp=(e)=>{
-        e.preventDefault();
+  const handleSignUp = async (e) => {
+    e.preventDefault();
 
-        const form=e.target;
-        const Email=form.email.value;
-        const Password=form.password.value;
-    
-        console.log(Email,Password);
-        createUser(Email,Password).then((userCredential) => {
-          // Signed up 
-          const user = userCredential.user;
-          navigate(from,{replace:true});
-          // ...
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
+    const { username, email, password } = formData;
 
-          if (errorCode === 'auth/email-already-in-use') {
-            setError("An account with this email already exists. Please log in.");
-          }
-          else{
-            setError(errorMessage);
-
-          }
-          
-
-
-
-          // ..
-        });
-      
-
-        
-
-
-    };
-
-
-    const handleRegister=()=>{
-      loginwithGoogle().then((result)=>{
-        const user=result.user;
-        navigate(from,{replace:true});
-
-      }).catch((error)=>{
-        const errorCode=error.code;
-        const errorMessage=error.message;
-        setError(errorMessage);
-      })
+    if (!username || !email || !password) {
+      return setError('Please fill out all fields.');
     }
 
-   
+    setLoading(true);
+    setError('');
 
-    
+    try {
+      const res = await fetch('http://localhost:3000/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        setLoading(false);
+        return setError(data.message || 'Something went wrong. Please try again.');
+      }
+
+      setLoading(false);
+      navigate(from, { replace: true });
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
 
   return (
-    <div className='row g-0 justify-content-center align-items-center login-container mt-3'>
-
-      <div className="col-10 row g-0  border rounded-2 bg-white mb-8">
-
+    <div className="row g-0 justify-content-center align-items-center login-container mt-3">
+      <div className="col-10 row g-0 border rounded-2 bg-white mb-8">
+        {/* Left Section */}
         <div className="d-none d-md-block col-6">
-          <img src="./login.jpg" alt="" className='img-fluid h-100'  />
+          <img src="./login.jpg" alt="" className="img-fluid h-100" />
         </div>
 
-        <form className='col-12 col-md-6 px-3 py-3' onSubmit={handleSignUp}>
+        {/* Form Section */}
+        <form className="col-12 col-md-6 px-3 py-3" onSubmit={handleSignUp}>
           
-          <div className='d-flex align-items-center justify-content-center'>
-            <h5 className=' text-center py-2 mb-1 fst-italic fw-lighter '>Discover, Explore, Imagine.</h5>
-            <AiFillBook></AiFillBook>
 
-          </div>
-          
-          {error && <div className="alert alert-danger text-center mb-3">{error}</div>} {/* Display error message  */}
-         
-          <h5 className='text-center fst-italic mb-3'>Sign Up</h5>
+          {error && <div className="alert alert-danger text-center mb-3">{error}</div>}
 
+          <h5 className="text-center fst-italic mb-3">Sign Up</h5>
+
+          {/* Username Input */}
           <div className="form-floating mb-3">
-            <input type="text" className='form-control rounded' id='username' placeholder='your name' required onChange={(e)=>{
-              setUsername(e.target.value) 
-            }}/>
+            <input
+              type="text"
+              className="form-control rounded"
+              id="username"
+              placeholder="Your name"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
             <label htmlFor="username">Username</label>
           </div>
+
+          {/* Email Input */}
           <div className="form-floating mb-3">
-            <input type="email" className='form-control rounded' id='email' placeholder='xyz.@gmail.com' required  onChange={(e)=>{
-              setEmail(e.target.value) 
-            }}/>
+            <input
+              type="email"
+              className="form-control rounded"
+              id="email"
+              placeholder="xyz.@gmail.com"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
             <label htmlFor="email">Email</label>
           </div>
 
+          {/* Password Input */}
           <div className="form-floating mb-3">
-            <input type="password" className='form-control rounded' id='password' placeholder='password' required  onChange={(e)=>{
-              setPassword(e.target.value) 
-            }}/>
+            <input
+              type="password"
+              className="form-control rounded"
+              id="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
             <label htmlFor="password">Password</label>
           </div>
 
+          {/* Submit Button */}
           <div className="text-center">
-            <button className=' btn login-btn py-2 px-2 rounded-3 d-flex align-items-center justify-content-center'>Sign up <CiLogin></CiLogin></button>
-          </div>
-
-
-
-          
-          <div className="text-center mt-3 ">
-            <button className='btn btn-outline-dark w-100 d-flex align-items-center justify-content-center' onClick={handleRegister}>
-              <AiFillGoogleCircle className='w-8 h-8 mr-3'></AiFillGoogleCircle>
-              Continue with Google
+            <button
+              type="submit"
+              className="btn login-btn py-2 px-2 rounded-3 d-flex align-items-center justify-content-center"
+              disabled={loading}
+            >
+              {loading ? (
+                <span>Loading...</span>
+              ) : (
+                <>
+                  Sign up <CiLogin />
+                </>
+              )}
             </button>
           </div>
 
-        
+          {/* Google OAuth */}
+          <OAuth></OAuth>
 
-
-
-
-          <div className="text-center mt-4">Already have an Account? <Link to="/login" className='loginLink'>Log in</Link></div>
-
-          <div className='text-center mt-4'>By continuing, I agree to the <Link className='Terms'> Terms of Use  </Link>& <Link className='Privacy'>Privacy Policy</Link></div>
-
+          {/* Footer Links */}
+          <div className="text-center mt-4">
+            Already have an Account? <Link to="/login" className="loginLink">Log in</Link>
+          </div>
+          <div className="text-center mt-4">
+            By continuing, I agree to the <Link className="Terms">Terms of Use</Link> & <Link className="Privacy">Privacy Policy</Link>
+          </div>
         </form>
-
-
       </div>
-
-
-      
-    
     </div>
-
-  )
+  );
 }
