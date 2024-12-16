17ad982923be13f51070b7d9d02afbfd7b5e76ad
13Anisha <119130741+13Anisha@users.noreply.github.com>
2024-12-16 19:59:19 +0530

    Update vite.config.js

--- SehatKal/vite.config.js
+++ SehatKal/vite.config.js
@@ -26,15 +26,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../server/public', // Output the build files to the server's public directory
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    historyApiFallback: true,  // Important for single-page apps (SPA) with React Router
+    outDir: '../server/SehatKal/dist', // Output React build files to the server's dist directory
   },
 });
 
