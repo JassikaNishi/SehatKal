ea91e8a4214cc23abf671de19c6c6298d9e1acd2
13Anisha <119130741+13Anisha@users.noreply.github.com>
2024-12-16 19:57:27 +0530

    Update vite.config.js

--- SehatKal/vite.config.js
+++ SehatKal/vite.config.js
@@ -6,18 +6,35 @@
 // });
 
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//   },
+//   server: {
+//     host: '0.0.0.0',
+//     port: 8000,
+//     historyApiFallback: true,
+//   },
+//   base: '/',
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: '../server/public', // Output the build files to the server's public directory
   },
   server: {
     host: '0.0.0.0',
-    port: 8000,
-    historyApiFallback: true,
+    port: 3000,
+    historyApiFallback: true,  // Important for single-page apps (SPA) with React Router
   },
-  base: '/',
 });
+
